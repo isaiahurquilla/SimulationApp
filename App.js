1f68dd19bf15ae1e04dc76ecc789ca28975cf314
Isaiah <isaiahurquilla@gmail.com>
2026-04-14 15:17:54 -0700

    download expo

---
+++ App.js
@@ -0,0 +1,6 @@
+import react, { useState, useEffect, useMemo} from "react";
+import { SafeAreaView, View, Tewxt, StyleSheet, TouchableOpacity, TextInput, ScrollView} from "react-native";
+
+export default function App() {
+  const [screen, setScreen] = useState("home");
+}
